f70e4e4f0bd08ed2706ca5ed58ad457febe6f7e0
Arturas Jankus <jankus.arturas@gmail.com>
2014-02-26 19:34:32 +0200

    Patobulinta parametrų nuskaitymo f-ja. Įdėtos minimalios apsaugos.

--- src/statistics.py
+++ src/statistics.py
@@ -5,13 +5,27 @@ import os
 import sys
 
 def getArguments():
-	if len(sys.argv) > 3: #argumentų skaičius negali būti didesnis nei 2
-		sys.exit("Per daug parametrų arba jie nurodyti nekorektiškai.")
-	if len(sys.argv) < 3: #argumentų skaičius negali būti mažesnis nei 2
-		sys.exit("Per mažai parametrų arba jie nurodyti nekorektiškai.")
+	if len(sys.argv) > 4: #argumentų skaičius negali būti didesnis nei 2
+		sys.exit("Per daug parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help me' - naudojimo instrukcija")
+	if len(sys.argv) < 4: #argumentų skaičius negali būti mažesnis nei 2
+		sys.exit("Per mažai parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help me' - naudojimo instrukcija")
+	if sys.argv[1] == "help" and sys.argv[2] == "me":
+		print("Naudojimas:  python statistics.py '/nuoroda/iki/aplanko/ištyrimui' '/nuoroda/iki/rezultatų/failo' 'failopavadinimas' ")
+		sys.exit()	
 
 	pathForAnalysis = sys.argv[1] #nuoroda iki norimo aplanko analizei
 	pathForResults = sys.argv[2] #nuoroda iki norimo aplanko bei failo pavadinimas, kuriame bus išsaugotas rezultatas
-	
-getArguments()
+	filename = sys.argv[3] #failo pavadinimas
+
+	if os.path.exists(pathForAnalysis) and os.path.exists(pathForResults):
+		return(pathForAnalysis, pathForResults, filename)
+	else:
+		sys.exit()
+
+pathForAnalysis, pathForResults, filename = getArguments()
 		
+def getFilesList(pathForAnalysis):
+	list = os.listdir(pathForAnalysis)
+	
+
+getFilesList(pathForAnalysis)
