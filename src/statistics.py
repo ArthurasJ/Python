006d6ae3296bf50fe497d68c70dd51806165461f
Arturas Jankus <jankus.arturas@gmail.com>
2014-02-28 00:20:08 +0200

    Metodas nuskaitantis tekstą iš failų ir analizuojantis simbolius išskaidytas į tris metodus - openFiles, kuris nuskaito tekstą bei analyseFileWords/Symbols, kurie analizuoja tekstą pagal simbolius/žodžius.

--- src/statistics.py
+++ src/statistics.py
@@ -8,9 +8,9 @@ def getArguments():
 	if sys.argv[1] == "help":
 		sys.exit("Naudojimas:  python statistics.py '/nuoroda/iki/aplanko/ištyrimui/' '/nuoroda/iki/rezultatų/failo/' 'failopavadinimas' ")
 	if len(sys.argv) > 4: #argumentų skaičius negali būti didesnis nei 2
-		sys.exit("Klaida: Per daug parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help me' - naudojimo instrukcija")
+		sys.exit("Klaida: Per daug parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help' - naudojimo instrukcija")
 	if len(sys.argv) < 4: #argumentų skaičius negali būti mažesnis nei 2
-		sys.exit("Klaida: Per mažai parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help me' - naudojimo instrukcija")
+		sys.exit("Klaida: Per mažai parametrų arba jie nurodyti nekorektiškai. 'Python statistics.py help' - naudojimo instrukcija")
 	
 	pathForAnalysis = sys.argv[1] #nuoroda iki norimo aplanko analizei
 	pathForResults = sys.argv[2] #nuoroda iki norimo aplanko bei failo pavadinimas, kuriame bus išsaugotas rezultatas
@@ -38,33 +38,69 @@ def getFilesList(pathForAnalysis):
 	listOfNames = [f for f in os.listdir(pathForAnalysis) if os.path.isfile(os.path.join(pathForAnalysis,f))]
 	return listOfNames
 
-def analyseFiles(listOfNames, frez):
-	listOfSymbols = {}
+def openFiles(listOfNames, frez):
 	TotListOfSymbols = {}
+	TotListOfWords = {}
 	for x in xrange(0, len(listOfNames)):
 		try:
 			f = open(pathForAnalysis + listOfNames[x], "r")
-			printStringToFile("\n Failas:"+ pathForAnalysis + listOfNames[x] + "\n", frez) 
+			printStringToFile("\n\n Failas:" + pathForAnalysis + listOfNames[x], frez) 
 		except IOerror:
-			sys.exit("Klaida:Nepavyko nuskaityti failo esančio direktorijoje")
+			sys.exit("Klaida: Nepavyko nuskaityti failo esančio direktorijoje")
 
 		fileText = f.read()
-		i = 0
-		while i < len(fileText):
-			if fileText[i] in listOfSymbols:
-				number = listOfSymbols[fileText[i]] + 1
-				listOfSymbols[fileText[i]] = number
-			else:
-				listOfSymbols[fileText[i]] = 1
-			i=i+1
+		TotListOfSymbols = analyseFileSymbols(fileText, frez, TotListOfSymbols)
+		TotListOfWords = analyseFileWords(fileText, frez, TotListOfWords)
+		f.close()
 
-		printStringToFile(str(listOfSymbols), frez)
-
-		for symbol in listOfSymbols:
-			TotListOfSymbols[symbol] = TotListOfSymbols.get(symbol, 0) + listOfSymbols[symbol]
-		listOfSymbols = {}
-	printStringToFile("\n Visame kataloge: \n", frez) 
+	printStringToFile("\n\n Visame kataloge simbolių dažnis: \n\n", frez) 
 	printStringToFile(str(TotListOfSymbols), frez)
+
+	printStringToFile("\n\n Visame kataloge žodžių dažnis: \n\n", frez) 
+	printStringToFile(str(TotListOfWords), frez)
+
+
+def analyseFileWords(fileText, frez, TotListOfWords):
+	listOfWords = {}
+
+	i = 0	
+	words = fileText.split(" ")
+	while i < len(words):
+		if words[i] in listOfWords:
+			number = listOfWords[words[i]] + 1
+			listOfWords[words[i]] = number
+		else:
+			listOfWords[words[i]] = 1
+		i = i + 1
+
+	printStringToFile('\n\n Žodžiai faile: \n\n', frez)
+	printStringToFile(str(listOfWords), frez)
+
+	for word in listOfWords:
+		TotListOfWords[word] = TotListOfWords.get(word, 0) + listOfWords[word]
+
+	return TotListOfWords
+
+
+def analyseFileSymbols(fileText, frez, TotListOfSymbols):
+	listOfSymbols = {}
+
+	i = 0
+	while i < len(fileText):
+		if fileText[i] in listOfSymbols:
+			number = listOfSymbols[fileText[i]] + 1
+			listOfSymbols[fileText[i]] = number
+		else:
+			listOfSymbols[fileText[i]] = 1
+		i = i + 1
+
+	printStringToFile('\n\n Simboliai faile: \n\n', frez)
+	printStringToFile(str(listOfSymbols), frez)
+
+	for symbol in listOfSymbols:
+		TotListOfSymbols[symbol] = TotListOfSymbols.get(symbol, 0) + listOfSymbols[symbol]
+
+	return TotListOfSymbols
 
 def printStringToFile(stringline, f):
 	f.write(stringline)
@@ -74,4 +110,5 @@ def printStringToFile(stringline, f):
 pathForAnalysis, pathForResults, filename = getArguments()
 f = createResultsFile(pathForResults, filename)
 listOfNames = getFilesList(pathForAnalysis)
-analyseFiles(listOfNames, f) 
+openFiles(listOfNames, f)
+
